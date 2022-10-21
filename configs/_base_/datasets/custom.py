1e0ed3fb42296d0cf398fc110f5119a845d15d00
andysu <andysu.cs10@nycu.edu.tw>
2022-10-21 11:06:38 +0800

    [modify]more color aug [modify]rotate 180

--- configs/_base_/datasets/custom.py
+++ configs/_base_/datasets/custom.py
@@ -1,7 +1,10 @@
 # dataset settings Only for test
 dataset_type = 'CustomDepthDataset'
 data_root = './dataset/endoscopy/'
-depth_scale = 65535 / 16
+depth_max = 16
+depth_min = 1e-3
+depth_scale = 65535 / depth_max
+
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size= (480, 480)
@@ -9,10 +12,10 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='DepthLoadAnnotations'),
     dict(type='NYUCrop', depth=True),
-    dict(type='RandomRotate', prob=0.5, degree=2.5),
+    dict(type='RandomRotate', prob=0.5, degree=180.),
     dict(type='RandomFlip', prob=0.5),
     dict(type='RandomCrop', crop_size=(480, 480)),
-    dict(type='ColorAug', prob=1, gamma_range=[0.9, 1.1], brightness_range=[0.75, 1.25], color_range=[0.9, 1.1]),
+    dict(type='ColorAug', prob=1, gamma_range=[0.7, 1.3], brightness_range=[0.5, 1.5], color_range=[0.7, 1.3]),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'depth_gt']),
@@ -41,15 +44,15 @@ data = dict(
         depth_scale=depth_scale,
         pipeline=train_pipeline,
         test_mode=False,
-        min_depth=1e-3,
-        max_depth=16),
+        min_depth=depth_min,
+        max_depth=depth_max),
     val=dict(
         type=dataset_type,
         data_root=data_root + 'val',
         depth_scale=depth_scale,
         pipeline=test_pipeline,
-        min_depth=1e-3,
-        max_depth=16,
+        min_depth=depth_min,
+        max_depth=depth_max,
         test_mode=False),
     test=dict(
         type=dataset_type,
@@ -57,7 +60,7 @@ data = dict(
         depth_scale=depth_scale,
         pipeline=test_pipeline,
         test_mode=False,
-        min_depth=1e-3,
-        max_depth=16))
+        min_depth=depth_min,
+        max_depth=depth_max))
 
 
